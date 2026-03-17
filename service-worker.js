1eaa45553d8fa912a404c57d2b0bd44057d0b59d
cciarmoli <claudio.ciarmoli@gmail.com>
2026-03-17 09:34:48 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,35 +1,30 @@
 
-const CACHE_NAME = 'cantina-vini-cache-v1';
-
+const CACHE_NAME = 'cantina-vini-full-v1';
 const FILES_TO_CACHE = [
   './',
   './index.html',
   './manifest.json',
   './icons/icon-192.png',
-  './icons/icon-512.png'
+  './icons/icon-512.png',
+  './splash/iphone-1170x2532.png',
+  './splash/iphone-1284x2778.png',
+  './splash/iphone-1125x2436.png',
+  './splash/iphone-1242x2688.png'
 ];
-
-// Install
 self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
   );
   self.skipWaiting();
 });
-
-// Activate
 self.addEventListener('activate', (event) => {
   event.waitUntil(
-    caches.keys().then((keys) =>
-      Promise.all(keys.map(k => (k !== CACHE_NAME ? caches.delete(k) : null)))
-    )
+    caches.keys().then(keys => Promise.all(keys.map(k => k !== CACHE_NAME ? caches.delete(k) : null)))
   );
   self.clients.claim();
 });
-
-// Fetch (cache-first fallback to network)
 self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request).then((resp) => resp || fetch(event.request))
+    caches.match(event.request).then(resp => resp || fetch(event.request))
   );
 });
